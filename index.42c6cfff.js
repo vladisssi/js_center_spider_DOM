f77fb0be5ff4474873db8cbdcb6f4133540d2781
Vladislav <njiuyfqqq123@gmail.com>
2024-07-17 03:57:07 +0300

    make build

---
+++ index.42c6cfff.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");function o(){var o=e.clientWidth,i=e.clientHeight,n=t.clientWidth,l=t.clientHeight;t.style.position="absolute",t.style.left="".concat((o-n)/2,"px"),t.style.top="".concat((i-l)/2,"px")}window.onload=o,window.onresize=o;
+//# sourceMappingURL=index.42c6cfff.js.map
